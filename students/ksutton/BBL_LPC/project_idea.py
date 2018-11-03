44cef7976d371b4a349b718524d58bc59be26029
kateesutt <42789282+kateesutt@users.noreply.github.com>
2018-11-03 17:22:21 -0400

    Update students/ksutton/BBL_LPC/project_idea.py

---
+++ students/ksutton/BBL_LPC/project_idea.py
@@ -0,0 +1,28 @@
+# data inputs
+treepoints
+street
+segment_cross_streets
+broadway malls shapefile
+
+# steps
+existing plantings
+grab treepoints
+decode the lat long
+intersect treepoints with broadway malls shapefile
+make some visualizations of the whole corridor
+pick the segments
+split the broadway malls shapefile by the segment cross streets
+select only the trees within one segment
+do some visualizations of the trees in that segment
+iterate for the rest of the segments
+
+
+future plantings
+grab the treepoints only of the future plantings on the whole corridor
+add the unique ID to the dot for each tree planting or pit restoration
+color according to the species
+make the close up maps of each segment
+    and add the extent of each zoomed in map to the overview
+
+
+
