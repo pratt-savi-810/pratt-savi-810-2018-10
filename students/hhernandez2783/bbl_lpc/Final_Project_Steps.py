212b56461ca107667805ae2cff843a4cba15f043
hhernandez2783 <44526460+hhernandez2783@users.noreply.github.com>
2018-11-03 17:26:17 -0400

    final project

---
+++ students/hhernandez2783/bbl_lpc/Final_Project_Steps.py
@@ -0,0 +1,32 @@
+Bridge maps
+
+The goal of my final project will be to automate the process of mapping & exporting bridge traffic volumes for a work report.
+The bridge traffic volume can be compiled with lat and long coordinates for where the In and Out traffic volumes will be positioned on the map.
+There will also be lat and long coordinates for the corners of the extent of each map. Steps below.
+
+Inputs
+    In Traffic Volume (for each bridge)
+    Out Traffic Volume (for each bridge)
+    In Lat (for each bridge)
+    Out Lat (for each bridge)
+    In Long (for each bridge)
+    Out Long (for each bridge)
+    Extent Left Upper Corner Lat
+    Extent Left Upper Corner Long
+    Extent Left Lower Corner Lat
+    Extent Left Lower Corner Long
+    Extent Right Lower Corner Lat
+    Extent Right Lower Corner Long
+    Extent Right Upper Corner Lat
+    Extent Right Upper Corner Long
+
+
+
+Output
+    create and export 3 maps of In & Out bridge traffic volumes with automation for future maps with possible inset
+
+Steps
+    Prepare excel file with volumes and coordinates
+    determine extents needed for maps
+    write code
+    loop process
