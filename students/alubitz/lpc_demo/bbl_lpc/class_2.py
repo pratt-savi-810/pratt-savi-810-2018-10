24b73bae0d64c763733315dbc2f1483c5e1eb9fa
Adam Lubitz <adam.s.lubitz@gmail.com>
2018-11-03 16:13:07 -0400

    Update students/alubitz/lpc_demo/bbl_lpc/class_2.py

---
+++ students/alubitz/lpc_demo/bbl_lpc/class_2.py
@@ -0,0 +1,54 @@
+import arcpy
+
+footprints_file = r'C:\Users\alubitz\Desktop\LPC_IL_HD_Bld_DB_10_19_18.shp'
+
+def select_bin_ids(
+        input_bin,  # this is the input bin id
+        input_footprints,  # this is the historic footprints file
+        distance,  # this is the buffer distance
+):
+    sql_bin = '''"BIN" = {}'''.format(input_bin)
+
+    if arcpy.Exists('build_lyr'):
+        arcpy.Delete_management('build_lyr')
+    if arcpy.Exists('select_bin_lyr'):
+        arcpy.Delete_management('select_bin_lyr')
+    if arcpy.Exists('in_memory/buffer'):
+        arcpy.Delete_management('in_memory/buffer')
+
+    arcpy.MakeFeatureLayer_management(
+        input_footprints,  # input feature class
+        "build_lyr",  # output layer
+        sql_bin,  # sql statement
+    )
+
+    arcpy.Buffer_analysis(
+        "build_lyr",
+        "in_memory/buffer",
+        distance,
+    )
+
+    arcpy.MakeFeatureLayer_management(
+        input_footprints,
+        'select_bin_lyr',
+    )
+
+    arcpy.SelectLayerByLocation_management(
+        'select_bin_lyr',
+        'INTERSECT',
+        'in_memory/buffer',
+    )
+
+    bin_list = []
+
+    with arcpy.da.SearchCursor('select_bin_lyr', 'BIN') as cursor:
+        for row in cursor:
+            bin_list.append(row[0])
+
+    arcpy.Delete_management('build_lyr')
+    arcpy.Delete_management('select_bin_lyr')
+    arcpy.Delete_management('in_memory/buffer')
+
+    return bin_list
+
+print (select_bin_ids('1011256', footprints_file, '400 feet'))
