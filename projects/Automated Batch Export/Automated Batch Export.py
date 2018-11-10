aa13cb9057143f1eaade82325e4b8dc51228488e
jaramana <jaramana_dam@hotmail.com>
2018-11-10 15:16:04 -0500

    Update projects/Automated Batch Export/Automated Batch Export.py

--- projects/Automated Batch Export/Automated Batch Export.py
+++ projects/Automated Batch Export/Automated Batch Export.py
@@ -1,11 +1,12 @@
 import arcpy.mapping
 
-Folder = "C:\Users\ashaiban\Desktop\Lesson 6\test"  # Alter this for your output folder
-mxd = arcpy.mapping.MapDocument("C:\Users\ashaiban\Desktop\Lesson 6\Untitled.mxd")
+Folder = "C:Users/ashaiban/Desktop/Lesson 6/test"  # Alter this for your output folder
+mxd = arcpy.mapping.MapDocument("C:/Users/ashaiban/Desktop/Lesson 6/Untitled.mxd")
 df = arcpy.mapping.ListDataFrames(mxd, "Layers")[0]
 
 for bkmk in arcpy.mapping.ListBookmarks(mxd, "", data_frame=df):
     df.extent = bkmk.extent
+    lyr = arcpy.mapping.ListLayers(mxd, "nybb", df)[0]
     dfname = bkmk.name
     arcpy.RefreshActiveView()
-    arcpy.mapping.ExportToPNG(mxd, Folder + "/" + OutText[:-11] + ".png", resolution=600)
+    arcpy.mapping.ExportToPNG(mxd, "C:/Users/ashaiban/Desktop/Lesson 6/test/" + "#", resolution=600)
