6b9e9b939fee58f3afc3e3cadbee1fe2bdc40680
Danny S <daniel.martin.sheehan@gmail.com>
2018-11-16 19:45:23 -0500

    clean

--- lessons/lesson_09_zoom_to_selected_features/02_lesson_09_zoom_to_selected_features.py
+++ lessons/lesson_09_zoom_to_selected_features/02_lesson_09_zoom_to_selected_features.py
@@ -35,15 +35,21 @@ def export_map_for_feature_in_fc(fc, mxd_file, unique_fieldname, output_png_dir)
                 field_value.replace('/', '').replace("""n\""", '')
             )
         )
+        
+        
 def feature_class_to_dataframe(in_fc):
     field_list = get_fc_fields(in_fc)
     df = pd.DataFrame([row for row in arcpy.da.SearchCursor(in_fc, field_list)])
     df.columns = field_list
     return df
     # check out https://joelmccune.com/arcgis-to-pandas-data-frame-v2-0/
+    
+    
 def get_fc_fields(fc):
     field_names = []
     fields = arcpy.ListFields(fc)
     for field in fields:
         field_names.append(field.name)
     return field_names
+
+
