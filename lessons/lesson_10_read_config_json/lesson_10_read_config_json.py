676feb2481be8acc2f95af3bf3f95fb9a901384e
Danny S. - Windows on MacBook Pro <daniel.martin.sheehan@gmail.com>
2018-11-16 20:28:21 -0500

    add prints and regular config.

--- lessons/lesson_10_read_config_json/lesson_10_read_config_json.py
+++ lessons/lesson_10_read_config_json/lesson_10_read_config_json.py
@@ -9,3 +9,15 @@ print(
     d['Neutral Milk Hotel']['On Avery Island']['year'],
     d['Neutral Milk Hotel']['On Avery Island']['record company'],
 )
+
+print(
+    d['Big Star'],
+)
+
+
+project_files = read_config('config.json')
+
+print(project_files)
+
+for i in project_files.items():
+    print(i)
