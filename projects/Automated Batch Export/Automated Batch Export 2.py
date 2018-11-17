13fba4ef89dd25ba06a3411f8e8b683800b18262
jaramana <jaramana_dam@hotmail.com>
2018-11-17 17:41:06 -0500

    group layer!

--- projects/Automated Batch Export/Automated Batch Export 2.py
+++ projects/Automated Batch Export/Automated Batch Export 2.py
@@ -1,28 +1,10 @@
 import arcpy
 
-projFolder = r"C:/Users/ashaiban/Desktop/Lesson 6"
 
-mxd = arcpy.mapping.MapDocument(r"C:/Users/ashaiban/Desktop/Lesson 6/Untitled.mxd".format(projFolder))
-df = arcpy.mapping.ListDataFrames(mxd, '')[0]
 
-PNGPath = r"C:/Users/ashaiban/Desktop/Lesson 6/test"
+mxd = arcpy.mapping.MapDocument("C:/Users/ashaiban/Desktop/Lesson 6/Untitled.mxd")
+df = arcpy.mapping.ListDataFrames(mxd, "")
+bkmk = arcpy.mapping.ListBookmarks(mxd, "")
+layers = arcpy.mapping.ListLayers(mxd, "")
 
-layers = ['nybb','buffer_400','LPC_IL_HD_Bld_DB_10_19_18'] # List of your output layers
-
-lyrlist = arcpy.mapping.ListLayers(mxd, '', df)
-
-for layer in layers:
-    layerOn = "*" # When required layer is turned on this will be the layer name to output
-    for lyr in lyrlist:
-        lyr.visible = False
-        #if lyr.name in layers and lyr.name <> layer:
-        #   lyr.visible = False
-        if lyr.name == "nybb":
-            lyr.visible = True
-        if lyr.name == "buffer_400":
-            lyr.visible = True
-        if lyr.name == layer:
-            lyr.visible = True
-            layerOn = lyr.name
-    if layerOn:
-        arcpy.mapping.ExportToPNG(mxd,r"C:/Users/ashaiban/Desktop/Lesson 6/test/%s.png" % lyr.name)
+print layers
--- projects/Automated Batch Export/Automated Batch Export.py
+++ projects/Automated Batch Export/Automated Batch Export.py
@@ -1,15 +1,46 @@
 import arcpy
 
+
+
 mxd = arcpy.mapping.MapDocument("C:/Users/ashaiban/Desktop/Lesson 6/Untitled.mxd")
-df = arcpy.mapping.ListDataFrames(mxd, "Layers")[0]
-lyr = arcpy.mapping.ListLayers(mxd, "Buffer", df)[0]
+df = arcpy.mapping.ListDataFrames(mxd, "*")
+bkmk = arcpy.mapping.ListBookmarks(mxd, "*")
+layers = arcpy.mapping.ListLayers(mxd, "*")
+
+#print lyr
+
+for item in layers:
+    print(item)
+    # print(item.isGroupLayer)
+    item.visible = False
+    if item.name == "IND":   #first layer
+        item.visible = True
+    if item.name == "LPC":   #second layer
+        item.visible = True
+    if item.name == 'Group One':   # third layer
+        item.visible = True
+    if item.name == 'nybb':  # third layer
+        item.visible = True
+
 arcpy.RefreshActiveView()
 arcpy.mapping.overwriteExport = True
-arcpy.mapping.ExportToPNG(mxd, r"C:/Users/ashaiban/Desktop/Lesson 6/test/test1231.png",
-                          resolution = 50)
+arcpy.mapping.ExportToPNG(mxd, r"C:/Users/ashaiban/Desktop/Lesson 6/test/test1.png")
+
+
+
+'''
+layers = ["IND"]
+
+for lyr in layers:
+    if lyr.name in layers:
+        lyr.visible = True
+
+
+arcpy.RefreshActiveView()
+arcpy.mapping.overwriteExport = True
+arcpy.mapping.ExportToPNG(mxd, r"C:/Users/ashaiban/Desktop/Lesson 6/test/test1.png")
+
+print lyr
 
 del mxd
-
-
-
-
+'''
