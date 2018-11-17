f38f47befdb362caeacd2bf06c9738730559c80d
ddouglasgoody <43116808+ddouglasgoody@users.noreply.github.com>
2018-11-17 13:35:03 -0500

    update

--- students/ddg/python_20181117/python_2018_11_17.py
+++ students/ddg/python_20181117/python_2018_11_17.py
@@ -2,16 +2,16 @@ import geocoder
 import pandas as pd
 import arcpy
 
-add_df = pd.read_csv(r'F:\Python Class\brooklyn_addresses_for_project_test.csv')
+add_df = pd.read_csv(r'C:\Users\dgoodma7\Documents\ArcGIS\Projects\ddg_project_20181117\brooklyn_addresses_for_project_test.csv')
 add_df['Lat'] = ''
 add_df['Long'] = ''
 
 for index, row in add_df.iterrows():
-    g = geocoder.google(row['Full Address'])
+    g = geocoder.osm(row['Full Address'])
     row['Lat'] = g.lat
     row['Long'] = g.lng
 
-add_df.to_csv(r'F:\Python Class\brooklyn_addresses_for_project_test.csv')
+add_df.to_csv(r'C:\Users\dgoodma7\Documents\ArcGIS\Projects\ddg_project_20181117\brooklyn_addresses_for_project_test.csv')
 
 arcpy.management.MakeXYEventLayer(
     'F:\Python Class\brooklyn_addresses_for_project_test.csv',
