0da000f099bc3973bc45cfc5a8c582cb5bf521ab
pjarymowycz <42789243+pjarymowycz@users.noreply.github.com>
2018-11-21 21:40:28 -0500

    Working code with some bugs
    Code is mostly working, but:
     - mxd not updating properly
       - only exporting a single map
       - need to refresh geodatabase connections

---
+++ projects/pjarymow_project/Bikeshare_Run.py
@@ -0,0 +1,9 @@
+import Bikeshare_Functions as Bf
+
+# Run Bikeshare Route Finder with various config.json files
+# Load different travel points and save destinations through config.json files
+
+# Bf.run_bikeshare_route('config1.json')
+# Bf.run_bikeshare_route('config2.json')
+# Bf.run_bikeshare_route('config3.json')
+Bf.run_bikeshare_route('config4.json')
