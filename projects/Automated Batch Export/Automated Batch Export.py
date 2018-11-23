e1f6b00ecfba0fd8453ab2af23c8a1fab1a50be0
jaramana_dam <jaramana_dam@hotmail.com>
2018-11-23 14:17:12 -0500

    Update 2
    11 / 23 / 2018; 04:17

--- projects/Automated Batch Export/Automated Batch Export.py
+++ projects/Automated Batch Export/Automated Batch Export.py
@@ -1,13 +1,76 @@
+#import modules
 import arcpy
 
+# set the mxd
 mxd = arcpy.mapping.MapDocument(r"C:\Users\blazer\Documents\Desktop\Python\Python.mxd")
+
+#import layers
 layers = arcpy.mapping.ListLayers(mxd, "*")
 
-for bkmk in arcpy.mapping.ListBookmarks(mxd, "CD103"): #Put in the Bookmark Name
+
+#the following would be for one export, copy for as many exports as needed
+#Export 1
+# input the desired bookmark name
+for bkmk in arcpy.mapping.ListBookmarks(mxd, "<BOOKMARK>"): #Bookmark
     ext = bkmk.extent
     df = arcpy.mapping.ListDataFrames(mxd)[0]
     df.extent = ext
     for item in layers:
-        if item.name == 'Year Built': #Put in the desired layers
+        # input the desired layers to be on
+        if item.name == '<LAYER>':
             item.visible = True
-    arcpy.mapping.ExportToPDF(mxd, r"C:\Users\blazer\Desktop\test3.pdf".format(bkmk.name)) #Export Destination
+        if item.name == '<LAYER>':
+            item.visible = True
+        if item.name == '<LAYER>':
+            item.visible = True
+
+    #export
+    arcpy.mapping.ExportToPDF(mxd, r"<DESTINATION>".format(bkmk.name)) #Export Destination
+
+
+#Export 2
+for bkmk in arcpy.mapping.ListBookmarks(mxd, "<BOOKMARK>"):
+    ext = bkmk.extent
+    df = arcpy.mapping.ListDataFrames(mxd)[0]
+    df.extent = ext
+    for item in layers:
+        if item.name == '<LAYER>':
+            item.visible = True
+        if item.name == '<LAYER>':
+            item.visible = True
+        if item.name == '<LAYER>':
+            item.visible = True
+
+    arcpy.mapping.ExportToPDF(mxd, r"<DESTINATION>".format(bkmk.name))
+
+
+#Export 3
+for bkmk in arcpy.mapping.ListBookmarks(mxd, "<BOOKMARK>"):
+    ext = bkmk.extent
+    df = arcpy.mapping.ListDataFrames(mxd)[0]
+    df.extent = ext
+    for item in layers:
+        if item.name == '<LAYER>':
+            item.visible = True
+        if item.name == '<LAYER>':
+            item.visible = True
+        if item.name == '<LAYER>':
+            item.visible = True
+
+    arcpy.mapping.ExportToPDF(mxd, r"<DESTINATION>".format(bkmk.name))
+
+
+#Export 4
+for bkmk in arcpy.mapping.ListBookmarks(mxd, "<BOOKMARK>"):
+    ext = bkmk.extent
+    df = arcpy.mapping.ListDataFrames(mxd)[0]
+    df.extent = ext
+    for item in layers:
+        if item.name == '<LAYER>':
+            item.visible = True
+        if item.name == '<LAYER>':
+            item.visible = True
+        if item.name == '<LAYER>':
+            item.visible = True
+
+    arcpy.mapping.ExportToPDF(mxd, r"<DESTINATION>".format(bkmk.name))
