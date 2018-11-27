e70ce73f78e3afa0faf34e0c2e5dc83c6d400993
derekljasmin <44526687+derekljasmin@users.noreply.github.com>
2018-11-26 20:04:11 -0500

    hi

--- students/djasmin/project.py
+++ students/djasmin/project.py
@@ -1,6 +1,6 @@
-
 buffer_dist = '150 Feet'
-streets_dir = 'E:/Python Project/Shapefiles/Streets'
+streets_dir = 'G:/Python Project/Shapefiles/Streets'
+tax_lots_dir = 'G:/Python Project/Shapefiles'
 
 far_file_list = [
 	['Wyckoff_Avenue', 3.6],
@@ -19,42 +19,51 @@ def buffer_street_apply_far(
         far,
         buffer,
 ):
-    arcpy.Buffer_analysis(
+        arcpy.Buffer_analysis(
         street,
         '{}'.format(street.replace('.shp', '_buf.shp')),
         buffer_dist,
-    )
-    
-    # dissolve tool name will be something like '{}'.format(street.replace('.shp', '_buf_dis.shp'))
-    # select bbl by location intersect w/ buffer
-    # for selected features calculate new far field
-    
-   
-    
-# add field to mappluto, called new_far
+        )
 
 
 for i in far_file_list:
-    street = i[0]
-    far = i[1]
-    buffer_street_apply_far(
-        '{0}/{1}.shp'.format(streets_dir, street), 
-        far, 
-        '150 Feet',
-    )
+        street = i[0]
+        far = i[1]
+        buffer_street_apply_far(
+            '{0}/{1}.shp'.format(streets_dir, street),
+            far,
+            '150 Feet',
+        )
+
+    # dissolve tool name will be something like '{}'.format(street.replace('.shp', '_buf_dis.shp'))
+    # select bbl by location intersect w/ buffer
+    # for selected features calculate new far field
+
 
 # dissolve function
 import arcpy
-arcpy.Dissolve_management("E:\Python Project\Shapefiles\Streets\Bushwick_Avenue_buf.shp",
-			  "E:\Python Project\Shapefiles\Streets\Bushwick_Avenue_dissolved.shp",
+arcpy.Dissolve_management("far_file_list[0]_buf.shp",
+			  "far_file_list[0]_dissolved.shp",
                           "",
 			  "SINGLE_PART", 
                           "DISSOLVE_LINES")
 
-# select bbl's where new far field is null (zip code 11237 = FAR 2.0; all others = FAR 1.35)
-# calculate selected bb's where new far field is null 
 
+for buffer, i in zip("far_file_list[0]_dissolved.shp", len("far_file_list[0]_dissolved.shp")):
+    arcpy.MakeFeatureLayer_management("tax_lots_dir_Bushwick_TaxLots.shp", "parcels_lyr_{0}".format(i))
 
+    arcpy.SelectLayerByLocation_management(
+         "parcel_lyr_{0}".format(i),
+         "INTERSECT",
+         "tax_lots_dir_Bushwick_TaxLots.shp".format(i)
+    )
 
+    arcpy.FeatureClassToFeatureClass_conversion(
+        "parcel_lyr",  # feature layer
+        "tax_lots_dir",   # output location
+        "Bushwick_NewLots.shp".format(i),  # output name
+   )
     
     
+# select bbl's where new far field is null (zip codes 11206, 11237 = FAR 2.0; 11207, 11221 = FAR 1.35)
+# calculate selected bb's where new_far field is null
